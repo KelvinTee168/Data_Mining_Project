07da81f8d86fe40ab4c03a3e8b5c9f32268d5c5e
KelvinTee168 <kelvin.tee168@gmail.com>
2022-04-02 23:24:31 +0800

    merge

--- .ipynb_checkpoints/home-checkpoint.py
+++ .ipynb_checkpoints/home-checkpoint.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def app():
     st.title("What's interesting about this Laundry Shop?")
+    st.image("laundry.webp")
     st.write("**Prepared By** - *(Tee Eng Leong, 1191201404), (Anirban Bala Pranto, 1181202317)*")
 
     st.write("This application showcases the exploration and modelling of data obtained from a Laundry Shop. The application has been developed as part of a project for **Data Mining (TDS 3301)** class at **Multimedia University, Cyberjaya**")
